32ee37f60dcd57f26c64e03364d166b33e657e77
ajisoubari <72975317+ajisoubari@users.noreply.github.com>
2022-07-05 04:40:30 +0700

    Add files via upload

---
+++ Random_Quote_Machine/index.js
@@ -0,0 +1,23 @@
+const QUOTEBANK = [{
+    quote: " Optimistis adalah salah satu kualitas yang lebih terkait dengan kesuksesan dan kebahagiaan daripada yang lain.",
+ author: "Brian Tracy"},
+{
+    quote: " Hidup bukanlah masalah memegang kartu yang bagus, tapi terkadang, memainkan kartu yang buruk dengan baik.",
+ author: "Jack London"},
+{
+    quote: " Hidup adalah petualangan yang berani atau tidak sama sekali.",
+ author: "Helen Keller"},
+{
+    quote: " Lakukan apa yang harus kamu lakukan sampai kamu dapat melakukan apa yang ingin kamu lakukan.",
+ author: "Oprah Winfrey"},
+{
+    quote: " Ciri khas orang sukses adalah mereka selalu berusaha keras untuk mempelajari hal-hal baru.",
+ author: "Carol S. Dweck"},
+{
+    quote: " Penghargaan paling tinggi bagi seorang pekerja keras bukanlah apa yang dia peroleh dari pekerjaan itu, tapi seberapa berkembang ia dengan kerja kerasnya itu.",
+ author: "John Ruskin"},
+{
+    quote: " Optimistis adalah salah satu kualitas yang lebih terkait dengan kesuksesan dan kebahagiaan daripada yang lain.",
+ author: "Brian Tracy"},
+
+]
